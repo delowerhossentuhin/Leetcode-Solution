1306fc31756a2d615135138ebfabc0b3d384039e
delowerhossentuhin <dhtuhinmcc@gmail.com>
2025-03-29 03:14:01 +0600

    Find Closest Number to Zero

---
+++ 2239.cpp
@@ -0,0 +1,32 @@
+//Find Closest Number to Zero
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int findClosestNumber(vector<int>& nums) {
+
+        vector<int> array2;
+      for(int i=0;i<nums.size();i++)
+      {
+         int a=nums[i];
+         if(a<0)a*=(-1);
+         array2.push_back(a);
+      }
+      auto a=min_element(array2.begin(),array2.end());
+      vector<int>final;
+      for(int i=0;i<array2.size();i++)
+      {
+         if(array2[i]==*a)
+         {
+            final.push_back(nums[i]);
+         }
+      }
+      auto b=max_element(final.begin(),final.end());
+      return *b;
+    }
+};
+
+int main()
+{
+
+}
